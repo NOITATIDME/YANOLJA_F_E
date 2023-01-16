73dc18b8cbfcc1f714d232a93837bfb71ecbe34c
NOITATIDME <chgt186@naver.com>
2023-01-16 23:01:47 +0900

    국내 숙박 메인 임포트

--- yarnullja-f-e/src/views/HomeView.vue
+++ yarnullja-f-e/src/views/HomeView.vue
@@ -2,17 +2,17 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <dam-main/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DamMain from './domestic/dam/DamMain.vue'; 
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    'dam-main':DamMain
   }
 }
 </script>
---
+++ yarnullja-f-e/src/views/domestic/dam/DamMain.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>국내 숙박 메인</h2>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+
+export default class DamMain extends Vue {
+
+}
+</script>
