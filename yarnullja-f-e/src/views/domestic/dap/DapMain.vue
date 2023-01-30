bbce95b8f2b340e80923d071b2df8ec00e4087ee
이지윤 <ekfhddl8@naver.com>
2023-01-30 23:43:23 +0900

    [ 지윤 ] font 및 div 구조 설정

--- yarnullja-f-e/src/views/domestic/dap/DapMain.vue
+++ yarnullja-f-e/src/views/domestic/dap/DapMain.vue
@@ -1,15 +1,48 @@
 <template>
   <div>
     <header-main />
-    <h2>국내 모텔/호텔 결제 메인</h2>
-    <!--<payment-card v-for="item in CardLength" v-bind:key="item" />-->
     <payment-card />
   </div>
+  <!-- Search Area -->
+  <div class="container-fluid" style="height: 150px; background-color: #F2F2F2;">
+      <h1 style="font-family: gmk; padding:50px;"> 추천, 인기, 가격... 등의 필터 자리 </h1>
+  </div>
+
+  <!-- Contents Area -->
+  <div class="container-fluid" >
+    <div class="row" style="height: 900px;">
+
+      <!-- check payment Area -->
+      <div class="col">
+          <!-- title Area -->
+            <div class = "container-fluid">
+              <div class="row" style="padding: 3%;">
+                <!-- back button -->
+                <div class="col col-lg-2">
+                  <button type="button" class="btn btn-outline-secondary"><h9 style="font-family: jal;">&lt;</h9></button>
+                </div>
+                <!-- title text -->
+                <div class="col">
+                  <h2 style="font-family: gmk;"> 확인 및 결제</h2>
+                </div>
+              </div>
+            </div>
+      </div>
+      <!-- Rate details Area -->
+      <div class="col" style="background-color:  #848484;">
+          <h1 style="padding:30%"> Component Area</h1>
+
+
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <script lang="ts">
 import HeaderMain from "../../common/header/HeaderMain.vue";
 import PaymentCard from "./components/PaymentCard.vue";
+
 
 export default {
   name: "DapMain",
@@ -19,3 +52,16 @@ export default {
   },
 };
 </script>
+
+<!-- 임시 스타일 -->
+<style>
+@font-face {
+  font-family: 'jal';
+  src: url('../../../assets/fonts/jalnan.ttf');
+}
+
+@font-face {
+  font-family: 'gmk';
+  src: url('../../../assets/fonts/GmarketSansTTFBold.ttf');
+}
+</style>
--- yarnullja-f-e/src/views/domestic/dap/components/PaymentCard.vue
+++ yarnullja-f-e/src/views/domestic/dap/components/PaymentCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-button variant="danger">Button</b-button>
+    <!-- <b-button variant="danger">Button</b-button> -->
   </div>
 </template>
 
