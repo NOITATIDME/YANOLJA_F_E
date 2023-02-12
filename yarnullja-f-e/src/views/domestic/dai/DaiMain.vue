e92261360c4f6be78a550269af32f541cfbef3c2
it_sohee <im.sohee.1213@gmail.com>
2023-02-12 15:22:22 +0900

    sohee-dai-commit

--- yarnullja-f-e/src/views/domestic/dai/DaiMain.vue
+++ yarnullja-f-e/src/views/domestic/dai/DaiMain.vue
@@ -3,15 +3,36 @@
     <header-main></header-main>
     <h2>국내 모텔/호텔 정보</h2>
   </div>
+  <!-- <slider-banner></slider-banner> -->
+  <quick-category></quick-category>
+  <live-commerce></live-commerce>
+  <top-place></top-place>
+  <weeky-top></weeky-top>
 </template>
 
 <script lang="ts">
-import HeaderMain from "../../common/header/HeaderMain.vue";
+//css
+import "@/css/dai.css" 
+import "@/css/font.css"
+
+//Header 
+import HeaderMain from "@/views/common/header/HeaderMain.vue";
+// import SliderBanner from "@/views/domestic/dai/components/SliderBanner.vue";
+import QuickCategory from "@/views/domestic/dai/components/QuickCategory.vue";
+import LiveCommerce from "@/views/domestic/dai/components/LiveCommerce.vue";
+import TopPlace from "@/views/domestic/dai/components/TopPlace.vue";
+import WeeklyTop from "@/views/domestic/dai/components/WeeklyTop.vue";
+
 
 export default {
   name: "DaiMain",
   components: {
     "header-main": HeaderMain,
-  },
+    // "slider-banner": SliderBanner,
+    "quick-category": QuickCategory,
+    "live-commerce": LiveCommerce,
+    "top-place": TopPlace,
+    "weeky-top": WeeklyTop
+  }
 };
 </script>
---
+++ yarnullja-f-e/src/views/domestic/dai/components/WeeklyTop.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="WeeklyTop" class="Container_768x68_no2">
+        <div class="Container_Top">
+            <p>
+                <span class="Container_Title">위클리 인기 TOP &emsp; </span><br>
+                <span class="Container_Desc">관심 지역 근처의 구매 많은 순 추천</span>
+            </p>
+            <router-link to="/" class="Top_Place_3_8_items">
+                <p><span class="Container_More">전체 보기</span></p>
+            </router-link>
+        </div>
+        <div class="Container_Button">
+            <!-- <button class="Category-button-select" @click="changeCategory">모텔</button> -->
+            <li>
+                <button class="Category-button-select" @click="changeCategory">모텔</button>
+                <button class="Category-button" @click="changeCategory">호텔</button>
+            </li>
+        </div>
+        <div>
+            <h1>{{ category }}</h1>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+export default {
+    name: "WeeklyTop",
+    components: {},
+    data() {
+        return {
+            category: "motel"
+        }
+    },
+    methods: {
+        changeCategory() {
+            console.log("motel");
+        }
+    }
+};
+</script>
