8b1a3a6edc618e7abee21dc11d3c40528e9474f6
it_sohee <im.sohee.1213@gmail.com>
2023-02-13 21:17:28 +0900

    Main 파일 폴더 변경

--- yarnullja-f-e/src/views/domestic/dai/DaiMain.vue
+++ yarnullja-f-e/src/views/domestic/dai/DaiMain.vue
@@ -1,38 +1,11 @@
 <template>
-  <div>
-    <header-main></header-main>
-    <h2>국내 모텔/호텔 정보</h2>
-  </div>
-  <!-- <slider-banner></slider-banner> -->
-  <quick-category></quick-category>
-  <live-commerce></live-commerce>
-  <top-place></top-place>
-  <weeky-top></weeky-top>
+    <div>
+        <header-main></header-main>
+        <h2>국내 모텔/호텔 정보</h2>
+    </div>
+    <!-- <slider-banner></slider-banner> -->
+    <quick-category></quick-category>
+    <live-commerce></live-commerce>
+    <top-place></top-place>
+    <weeky-top></weeky-top>
 </template>
-
-<script lang="ts">
-//css
-import "@/css/dai.css" 
-import "@/css/font.css"
-
-//Header 
-import HeaderMain from "@/views/common/header/HeaderMain.vue";
-// import SliderBanner from "@/views/domestic/dai/components/SliderBanner.vue";
-import QuickCategory from "@/views/domestic/dai/components/QuickCategory.vue";
-import LiveCommerce from "@/views/domestic/dai/components/LiveCommerce.vue";
-import TopPlace from "@/views/domestic/dai/components/TopPlace.vue";
-import WeeklyTop from "@/views/domestic/dai/components/WeeklyTop.vue";
-
-
-export default {
-  name: "DaiMain",
-  components: {
-    "header-main": HeaderMain,
-    // "slider-banner": SliderBanner,
-    "quick-category": QuickCategory,
-    "live-commerce": LiveCommerce,
-    "top-place": TopPlace,
-    "weeky-top": WeeklyTop
-  }
-};
-</script>
