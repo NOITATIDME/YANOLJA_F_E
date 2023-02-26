ca4794330ac123fd20864221704e2f62db976cf9
NOITATIDME <chgt186@naver.com>
2023-02-27 00:11:19 +0900

    [예약내역] 예약내역 카드 퍼블리싱 중

--- yarnullja-f-e/src/views/manage/mmr/components/ReservationCard.vue
+++ yarnullja-f-e/src/views/manage/mmr/components/ReservationCard.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>
-    <h2>예약정보 카드</h2>
-    <b-button variant="outline-primary">Button</b-button>
+  <div class="Container_768x68_no1">
+    <div class="Container_Top">
+      <p>
+        <span class="Container_Title">날짜</span>
+        <span class="Container_More">상세보기></span>
+      </p>
+    </div>
+    <div class="Card_Container">
+      <div class="Card_left">
+        <img src="@/assets/images/yeoksam-hotelstar.png" />
+      </div>
+      <div class="Card_right"></div>
+    </div>
+  </div>
+  <div class="card-group">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          This is a wider card with supporting text below as a natural lead-in
+          to additional content. This content is a little bit longer.
+        </p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          This card has supporting text below as a natural lead-in to additional
+          content.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,3 +40,14 @@ export default {
   name: "ReservationCard",
 };
 </script>
+
+<style scoped>
+div.Card_Container {
+  max-width: 76.8rem;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-justify-content: space-between;
+  position: relative;
+  padding: 1rem 3rem 1rem 3rem;
+}
+</style>
